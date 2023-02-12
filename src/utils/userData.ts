c39ffec1f39a30f533fb9ce145e28647baef0ecf
ThiagoDalto <56813158+ThiagoDalto@users.noreply.github.com>
2023-02-12 10:59:26 -0300

    Feat: first commit

---
+++ src/utils/userData.ts
@@ -0,0 +1,7 @@
+export const userData = {
+  nameUser: "Thiago Dalto",
+  githubUser: "ThiagoDalto",
+  linkedinUser: "thiago-dalto-jovita",
+  whatsappNumber: "+5521970074931",
+  emailUser: "thiago.dalto@gmail.com",
+};
